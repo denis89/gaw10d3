718b337ba5a3293e75b00417de4891a350f4bee5
Denis Doan <dedoin08@yahoo.de>
2015-10-14 23:11:43 +0100

    It's not possible to use 2 functions in the Controller. At least the 1st commit works.

--- js/jsonController.js
+++ js/jsonController.js
@@ -1,7 +1,22 @@
 angular
    .module('jsonApp')
-  .controller('JsonController', function(JsonFactory) {
-  var self = this;
+  .controller('JsonController',
+       JsonCrudController,JsonController); 
+var self = this;
+   JsonCrudController.$inject = ['$resource'];
+    
+  function JsonCrudController($resource) {
+  
+  var Json = $resource('http://localhost:3000/jsons/:id', { id: '@_id' });
+    self.jsons = Json.query();
+
+    this.deleteJson = function(json) {
+    Json.delete({ id: json._id });
+    var index = self.jsons.indexOf(json);
+    self.jsons.splice(index, 1);
+    }
+
+  function JsonController(JsonFactory){
 
   JsonFactory.getPullRequests()
     .then(function(response) {
@@ -14,4 +29,5 @@ angular
     .then(function(response) {
       self.issues = response;
     });
-});
+  }
+};
